cbb73b2cc8c993f386e0409ce85b94105311bd0c
Daniel Braz <dsbraz@dsbraz.com>
2026-03-01 00:39:31 -0300

    Fix indents.scm: simplify indent queries, fix @{ anonymous block pattern

--- extension/languages/razor/indents.scm
+++ extension/languages/razor/indents.scm
@@ -1,77 +1,11 @@
-; =============================================================================
-; Razor Indentation Queries
-; =============================================================================
-
-; HTML elements increase indent
+; HTML elements
 (html_element) @indent.begin
 (html_self_closing_element) @indent.flat
 
-; C# code blocks
-(code_block
-  "{" @indent.begin
-  "}" @indent.end)
+; Opening braces — "@{" is a single token in razor_anonymous_block
+["@{" "{"] @indent.begin
+["}"] @indent.end
 
-(functions_block
-  "{" @indent.begin
-  "}" @indent.end)
-
-(section_block
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_anonymous_block
-  "{" @indent.begin
-  "}" @indent.end)
-
-; Control structure braces
-(razor_if
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_else
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_else_if
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_foreach
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_for
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_while
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_do_while
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_switch
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_try
-  "{" @indent.begin
-  "}" @indent.end)
-
-(catch_clause
-  "{" @indent.begin
-  "}" @indent.end)
-
-(finally_clause
-  "{" @indent.begin
-  "}" @indent.end)
-
-(razor_lock
-  "{" @indent.begin
-  "}" @indent.end)
-
-; Comments should not affect indentation
+; Comments don't affect indentation
 (html_comment) @indent.ignore
 (razor_comment) @indent.ignore
