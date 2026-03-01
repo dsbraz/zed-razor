c70c1bf6edbecbac8f0c997c9bfc1fe1af63537b
Daniel Braz <dsbraz@dsbraz.com>
2026-03-01 01:06:04 -0300

    Fix injections: use injection.content/@injection.content and injection.language format

--- extension/languages/razor/injections.scm
+++ extension/languages/razor/injections.scm
@@ -1,96 +1,81 @@
-; =============================================================================
-; Razor Language Injection Queries
-; Injects C# highlighting into code blocks and expressions
-; =============================================================================
+(code_block
+  (csharp_code) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into @code { ... } blocks
-((code_block
-  (csharp_code) @content)
-  (#set! "language" "CSharp"))
+(functions_block
+  (csharp_code) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into @functions { ... } blocks
-((functions_block
-  (csharp_code) @content)
-  (#set! "language" "CSharp"))
+(razor_anonymous_block
+  (csharp_code) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into anonymous @{ ... } blocks
-((razor_anonymous_block
-  (csharp_code) @content)
-  (#set! "language" "CSharp"))
+(razor_if
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into control structure conditions/expressions
-((razor_if
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_foreach
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_foreach
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_for
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_for
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_while
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_while
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_do_while
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_do_while
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_switch
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_switch
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_lock
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_lock
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(razor_using_statement
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_using_statement
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(catch_clause
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((catch_clause
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(switch_case
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((switch_case
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(explicit_expression
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into explicit expressions @(...)
-((explicit_expression
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
+(implicit_expression
+  (implicit_expression_content) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into implicit expressions @variable.Property
-((implicit_expression
-  (implicit_expression_content) @content)
-  (#set! "language" "CSharp"))
+(razor_await
+  (implicit_expression_content) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into @await expressions
-((razor_await
-  (implicit_expression_content) @content)
-  (#set! "language" "CSharp"))
+(expression_attribute_value
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-; Inject C# into expression attribute values
-((expression_attribute_value
-  (csharp_expression) @content)
-  (#set! "language" "CSharp"))
-
-; Inject CSS into <style> tags
-((html_element
+(html_element
   (html_start_tag
     name: (tag_name) @_tag)
-  (text_content) @content)
+  (text_content) @injection.content
   (#eq? @_tag "style")
-  (#set! "language" "css"))
+  (#set! injection.language "css"))
 
-; Inject JavaScript into <script> tags
-((html_element
+(html_element
   (html_start_tag
     name: (tag_name) @_tag)
-  (text_content) @content)
+  (text_content) @injection.content
   (#eq? @_tag "script")
-  (#set! "language" "javascript"))
+  (#set! injection.language "javascript"))
