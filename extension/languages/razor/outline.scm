7211c80b847f903d5716c7ba7120edd7cd1fcf8a
Daniel Braz <dsbraz@dsbraz.com>
2026-03-01 00:50:55 -0300

    Fix outline.scm: replace compound @keyword tokens with split keyword nodes

--- extension/languages/razor/outline.scm
+++ extension/languages/razor/outline.scm
@@ -1,31 +1,15 @@
-; =============================================================================
-; Razor Outline Queries
-; Used for the outline panel / symbol navigator
-; =============================================================================
-
-; @code blocks
-(code_block
-  "@code" @context) @item
-
-; @functions blocks
-(functions_block
-  "@functions" @context) @item
+; @code / @functions blocks
+(code_block "code" @name) @item
+(functions_block "functions" @name) @item
 
 ; Directives
-(directive_page
-  "@page" @context) @item
+(directive_page "page" @name) @item
+(directive_model "model" @context (directive_argument) @name) @item
+(directive_using "using" @context (directive_argument) @name) @item
+(directive_inject "inject" @context (directive_argument) @name) @item
 
-(directive_model
-  "@model" @context
-  (directive_argument) @name) @item
-
-(directive_using
-  "@using" @context
-  (directive_argument) @name) @item
-
-(directive_inject
-  "@inject" @context
-  (directive_argument) @name) @item
+; @section blocks
+(section_block "section" @context name: (identifier) @name) @item
 
 ; Top-level HTML elements
 (html_element
@@ -39,8 +23,3 @@
     "<" @context
     name: (tag_name) @name)) @item
   (#match? @name "^[A-Z]"))
-
-; @section blocks
-(section_block
-  "@section" @context
-  name: (identifier) @name) @item
