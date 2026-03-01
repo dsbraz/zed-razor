6f0601bcbc7801bbb392b54752cd025e4118b65f
Daniel Braz <dsbraz@dsbraz.com>
2026-03-01 01:21:24 -0300

    Fix injections: new format + remove @embedded from highlights

--- tree-sitter-razor/queries/injections.scm
+++ tree-sitter-razor/queries/injections.scm
@@ -1,21 +1,19 @@
-; Default injections for tree-sitter-razor
+(code_block
+  (csharp_code) @injection.content
+  (#set! injection.language "csharp"))
 
-((code_block
-  (csharp_code) @content)
-  (#set! "language" "c_sharp"))
+(functions_block
+  (csharp_code) @injection.content
+  (#set! injection.language "csharp"))
 
-((functions_block
-  (csharp_code) @content)
-  (#set! "language" "c_sharp"))
+(razor_anonymous_block
+  (csharp_code) @injection.content
+  (#set! injection.language "csharp"))
 
-((razor_anonymous_block
-  (csharp_code) @content)
-  (#set! "language" "c_sharp"))
+(explicit_expression
+  (csharp_expression) @injection.content
+  (#set! injection.language "csharp"))
 
-((explicit_expression
-  (csharp_expression) @content)
-  (#set! "language" "c_sharp"))
-
-((implicit_expression
-  (implicit_expression_content) @content)
-  (#set! "language" "c_sharp"))
+(implicit_expression
+  (implicit_expression_content) @injection.content
+  (#set! injection.language "csharp"))
